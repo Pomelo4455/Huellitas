f0b19835534d5a786f820a977f9ae0d2673224a9
Alucard-P <paulmct93@gmail.com>
2023-01-31 15:43:39 -0500

    update api

---
+++ api/app.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+// const recipes = require("./routes/recipes.js");
+// const diets = require("./routes/diets.js");
+
+require("./db.js");
+
+const server = express();
+
+server.name = "API";
+
+//edicion mia del json
+server.use(express.json());
+
+server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+server.use(bodyParser.json({ limit: "50mb" }));
+server.use(cookieParser());
+server.use(morgan("dev"));
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
+
+// server.use("/recipes", recipes);
+
+// server.use("/diets", diets);
+
+// Error catching endware.
+server.use((err, req, res, next) => {
+  // eslint-disable-line no-unused-vars
+  const status = err.status || 500;
+  const message = err.message || err;
+  console.error(err);
+  res.status(status).send(message);
+});
+
+module.exports = server;
