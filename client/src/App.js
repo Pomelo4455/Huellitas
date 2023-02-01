61e6839dbf7cfb86e70ffb9df46bce57d7c7e139
Lepemias <jeresegovia@yahoo.com.ar>
2023-02-01 16:55:33 -0300

    Sidebar & Login

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
-import Landing from './components/Landing';
+// import Login from './componentes/Login/Login';
 import './App.css';
+import Sidebar from './componentes/Sidebar/Sidebar';
 
 function App() {
   return (
 
 <Routes>
-<Route path="/" element={<Landing/>} />
+<Route path="/" element={<Sidebar/>} />
 </Routes>
 
   );
