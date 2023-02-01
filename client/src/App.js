a9f904c6a428c2c15b34ea820e7125ee43791385
NazaMaestre <Nazarenomaestre6@gmail.com>
2023-02-01 18:10:04 -0300

    SobreNosotros

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Footer from "./components/Footer/Footer";
 import Landing from "./components/Landing/Landing";
 import NavBar from "./components/NavBar/NavBar";
 import "./App.css";
+import SobreNosotros from "./components/About/SobreNosotros";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/Footer" element={<Footer />} />
+        <Route path="/SobreNosotros" element={<SobreNosotros />} />
         {/* <Route path="/home" element={<Home/>} /> */}
       </Routes>
     </>
