fef61bc6d89ac7e46cbe9204ea2579a4b196bddc
NazaMaestre <Nazarenomaestre6@gmail.com>
2023-02-02 18:11:40 -0300

    Fix visuales

--- client/src/data.js
+++ client/src/data.js
@@ -1,29 +1,32 @@
 export const pets = [
-    {
-        id: (Math.random()*3).toFixed(5),
-        image: 'https://s.yimg.com/ny/api/res/1.2/LRESSShwNplSJ7gFxqHtmQ--/YXBwaWQ9aGlnaGxhbmRlcjt3PTY0MDtoPTQ3Nw--/https://media.zenfs.com/en/homerun/feed_manager_auto_publish_494/5f2acfff720e9a2c822eaa0b4f37e3dd',
-        name: 'Mancha',
-        age: '3 años',
-        size: 'mediano',
-        color: 'blanco y negro',
-        description: 'Mancha es un perrito muy sociable',
-    },
-    {
-        id: (Math.random()*3).toFixed(5),
-        image: 'https://w7.pngwing.com/pngs/933/627/png-transparent-puppy-puppy-animal-puppy-clipart-thumbnail.png',
-        name: 'Manchita',
-        age: '1 año',
-        size: 'pequeño',
-        color: 'blanco y marron',
-        description: 'Manchita no es un perrito muy sociable',
-    },
-    {
-        id: (Math.random()*3).toFixed(5),
-        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKwpY3T06WzUOn-QqOYUDwMfWXlOGZNN6BAA&usqp=CAU',
-        name: 'Manchon',
-        age: '3 semanas',
-        size: 'Muy pequeño',
-        color: 'blanco y negro',
-        description: 'Mancho es un perrito recien nacido',
-    },
-]
+  {
+    id: (Math.random() * 3).toFixed(5),
+    image:
+      "https://s.yimg.com/ny/api/res/1.2/LRESSShwNplSJ7gFxqHtmQ--/YXBwaWQ9aGlnaGxhbmRlcjt3PTY0MDtoPTQ3Nw--/https://media.zenfs.com/en/homerun/feed_manager_auto_publish_494/5f2acfff720e9a2c822eaa0b4f37e3dd",
+    name: "Mancha",
+    age: "3 años",
+    size: "mediano",
+    color: "blanco y negro",
+    description: "Mancha es un perrito muy sociable",
+  },
+  {
+    id: (Math.random() * 3).toFixed(5),
+    image:
+      "https://w7.pngwing.com/pngs/933/627/png-transparent-puppy-puppy-animal-puppy-clipart-thumbnail.png",
+    name: "Manchita",
+    age: "1 año",
+    size: "pequeño",
+    color: "blanco y marron",
+    description: "Manchita no es un perrito muy sociable",
+  },
+  {
+    id: (Math.random() * 3).toFixed(5),
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKwpY3T06WzUOn-QqOYUDwMfWXlOGZNN6BAA&usqp=CAU",
+    name: "Manchon",
+    age: "3 semanas",
+    size: "Muy pequeño",
+    color: "blanco y negro",
+    description: "Mancho es un perrito recien nacido",
+  },
+];
