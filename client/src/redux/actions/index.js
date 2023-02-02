cb28cc7ca4fa9d3c735816104baeccfd4b7fb574
Agustin Fraile <agustinfraile98@gmail.com>
2023-02-02 17:37:40 -0300

    creando redux

---
+++ client/src/redux/actions/index.js
@@ -0,0 +1,47 @@
+import axios from 'axios';
+
+export const GET_PETS = 'GET_PETS';
+export const GET_PETS_DETAIL = 'GET_PETS_DETAIL';
+export const GET_PET_NAME = 'GET_PET_NAME'; 
+
+export const getPets = () => {
+    return async function(dispatch) {
+        try {
+            const pets = await axios.get('http://localhost:3001/pets');
+            return dispatch({
+                type: GET_PETS,
+                payload: pets
+            })
+        } catch (error) {
+            console.log(error)
+        }
+    }
+};
+
+export const getPetsDetail = (id) => {
+    return async function(dispatch) {
+        try {
+            const data = await axios.get(`http://localhost:3001/pets/${id}`);
+            return dispatch({
+                type: GET_PETS_DETAIL,
+                payload: data
+            });
+        } catch (error) {
+            console.log(error)
+            return dispatch ({
+                type: GET_PETS_DETAIL,
+                payload: []
+            })
+        };
+    };
+};
+
+export const getPetName = (name) => {
+    return async function(dispatch){
+        try {
+            
+        } catch (error) {
+            
+        }
+    };
+};
