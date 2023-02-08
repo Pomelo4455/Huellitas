482bd50123185edaaff436617cf2729ed315b790
Alucard-P <paulmct93@gmail.com>
2023-02-08 18:33:47 -0500

    DashBoard Adm

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -10,15 +10,16 @@ export const RESTORE_FILTERS = "RESTORE_FILTERS";
 export const GET_CAMPAIGNS = "GET_CAMPAIGNS";
 export const GET_DETAIL_CAMP = "GET_DETAIL_CAMP";
 export const SET_CURRENT_PAGE = "SET_CURRENT_PAGE";
-export const SEND_PROFILE_TO_DB= "SEND_PROFILE_TO_DB";
-export const CLEAR_PROFILE = "CLEAR_PROFILE"
-export const LOGIN_SUCCESS="LOGIN_SUCCESS"
-export const LOGIN_FAILURE="LOGIN_FAILURE"
-export const ADD_PROFILE="ADD_PROFILE"
-export const REMOVE_PROFILE="REMOVE_PROFILE"
-export const SET_DB_PROFILE="SET_DB_PROFILE"
-export const REMOVE_DB_PROFILE="REMOVE_DB_PROFILE"
-export const GET_FUNDACIONES = "GET_FUNDACIONES"
+export const SEND_PROFILE_TO_DB = "SEND_PROFILE_TO_DB";
+export const CLEAR_PROFILE = "CLEAR_PROFILE";
+export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+export const LOGIN_FAILURE = "LOGIN_FAILURE";
+export const ADD_PROFILE = "ADD_PROFILE";
+export const REMOVE_PROFILE = "REMOVE_PROFILE";
+export const SET_DB_PROFILE = "SET_DB_PROFILE";
+export const REMOVE_DB_PROFILE = "REMOVE_DB_PROFILE";
+export const GET_FUNDACIONES = "GET_FUNDACIONES";
+export const GET_USERS = "GET_USERS";
 
 export const getPets = () => {
   return async function (dispatch) {
@@ -61,29 +62,28 @@ export const getPetName = (name) => {
   };
 };
 
-export const sendProfileToDb =(prof)=>{
+export const sendProfileToDb = (prof) => {
   // console.log(prof)
   // let prof=JSON.parse(localStorage.getItem('user'))
   // console.log(prof)
-  return async function (){
-    try{
-      let loggedUser =await axios.post("http://localhost:3001/users", prof)
-      localStorage.setItem('loggedUser', JSON.stringify(loggedUser))
+  return async function () {
+    try {
+      let loggedUser = await axios.post("http://localhost:3001/users", prof);
+      localStorage.setItem("loggedUser", JSON.stringify(loggedUser));
       // console.log(profile)
       // return dispatch({
       //   type: SEND_PROFILE_TO_DB,
       //   payload: profile
       // })
-    }catch(error){
-      console.log(error.message)
+    } catch (error) {
+      console.log(error.message);
     }
-  }
-}
+  };
+};
 
-export const clearProfile = ()=>{
-  return { type: CLEAR_PROFILE
-  }
-}
+export const clearProfile = () => {
+  return { type: CLEAR_PROFILE };
+};
 
 export function postNewPet(payload) {
   return async function () {
@@ -119,20 +119,20 @@ export function updateFilters(filtros) {
   return { type: UPDATE_FILTERS, payload: filtros };
 }
 
-export function getCampaigns () {
+export function getCampaigns() {
   return async function (dispatch) {
     try {
-      const response = await axios.get('http://localhost:3001/campaigns');
+      const response = await axios.get("http://localhost:3001/campaigns");
       const allCampaigns = response.data;
 
       return dispatch({
         type: GET_CAMPAIGNS,
-        payload: allCampaigns
-      })
-    } catch(error) {
+        payload: allCampaigns,
+      });
+    } catch (error) {
       console.log(error);
     }
-  }
+  };
 }
 
 export const getDetailCamp = (id) => {
@@ -140,25 +140,28 @@ export const getDetailCamp = (id) => {
     try {
       const response = await axios.get(`http://localhost:3001/campaigns/${id}`);
       const campaign = response.data;
-      console.log(campaign)
+      console.log(campaign);
 
       return dispatch({
         type: GET_DETAIL_CAMP,
-        payload: campaign
-      })
-    } catch(error) {
+        payload: campaign,
+      });
+    } catch (error) {
       console.log(error.message);
     }
-  }
-}
+  };
+};
 
 export function postNewCampaign(payload) {
   return async function () {
     try {
-      const newCampaign = await axios.post("http://localhost:3001/campaigns", payload);
+      const newCampaign = await axios.post(
+        "http://localhost:3001/campaigns",
+        payload
+      );
 
       return newCampaign;
-    } catch(error) {
+    } catch (error) {
       console.log(error);
     }
   };
@@ -171,53 +174,54 @@ export function setCurrentPage(pageNumber) {
   };
 }
 
-
 ////
 export const login_success = () => {
   return {
-    type: LOGIN_SUCCESS
-  }
-}
+    type: LOGIN_SUCCESS,
+  };
+};
 
 export const login_failure = () => {
   return {
-    type: LOGIN_FAILURE
-  }
-}
-
+    type: LOGIN_FAILURE,
+  };
+};
 
 export const add_profile = (profile) => {
   return {
     type: ADD_PROFILE,
-    payload: profile
-  }
-}
+    payload: profile,
+  };
+};
 
 export const remove_profile = () => {
   return {
-    type: REMOVE_PROFILE
-  }
-}
-
+    type: REMOVE_PROFILE,
+  };
+};
 
 export const set_db_profile = (profile) => {
   return {
     type: SET_DB_PROFILE,
-    payload: profile
-  }
-}
+    payload: profile,
+  };
+};
 
 export const remove_db_profile = () => {
   return {
-    type: REMOVE_DB_PROFILE
-  }
-}
+    type: REMOVE_DB_PROFILE,
+  };
+};
 
 export function donate(payload) {
   return async function () {
     try {
-      const donation = await axios.post("http://localhost:3001/payment", payload)
-      .then((response)=> window.location.href = response.data.response.body.init_point)
+      const donation = await axios
+        .post("http://localhost:3001/payment", payload)
+        .then(
+          (response) =>
+            (window.location.href = response.data.response.body.init_point)
+        );
     } catch (error) {
       console.log(error);
     }
@@ -227,7 +231,9 @@ export function donate(payload) {
 export const getFundaciones = () => {
   return async function (dispatch) {
     try {
-      const fundaciones = await axios.get("http://localhost:3001/users?type=fundaciones");
+      const fundaciones = await axios.get(
+        "http://localhost:3001/users?type=fundaciones"
+      );
       return dispatch({
         type: GET_FUNDACIONES,
         payload: fundaciones.data,
@@ -235,5 +241,16 @@ export const getFundaciones = () => {
     } catch (error) {
       console.log(error);
     }
+  };
+};
+export const getUsers = () => {
+  return async function (dispatch) {
+    try {
+      const users = await axios.get("http://localhost:3001/users");
+      return dispatch({
+        type: GET_USERS,
+        payload: users.data,
+      });
+    } catch (error) {}
   };
 };
