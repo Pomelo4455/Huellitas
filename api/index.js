9f868c7339bc0137db88ae53d3592044be4610fa
Tomás Albanesi <tmsalbanesi@gmail.com>
2023-02-15 20:51:24 -0300

    solucionado por fin

--- api/index.js
+++ api/index.js
@@ -23,15 +23,6 @@ const app = require("./app.js");
 const { conn } = require("./db.js");
 const port = process.env.PORT || 3001;
 
-// Syncing all the models at once.
-// conn.sync({ force: true });
-
-conn.sync({ force: false }).then(() => {
-  server.listen(port, () => {
-    console.log(`%s listening at ${port}`); // eslint-disable-line no-console
-  });
-});
-
 // WEBSOCKETS CONFIG
 const SocketIO = require("socket.io");
 const http = require("http");
@@ -53,7 +44,7 @@ io.on('connection', (socket) => {
 // Syncing all the models at once.
 // conn.sync({ force: true });
 
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
   server.listen(port, () => {
     console.log(`%s listening at ${port}`); // eslint-disable-line no-console
   });
