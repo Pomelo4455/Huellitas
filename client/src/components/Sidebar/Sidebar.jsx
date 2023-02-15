d0deb0f21f4dc46c29624873308ec3da54bc5b0c
Pomelo4455 <sm67588@gmail.com>
2023-02-15 19:48:30 -0300

    Sidebar desplegable en dispositivos mobiles

--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -2,13 +2,13 @@ import Ordenamientos from "./Ordenamientos";
 import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { getFilterPets, restoreSearch } from "../../redux/actions";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { Icon } from "@iconify/react";
 import {
   combinarFiltros,
   handleCleanFilter,
   handleSelectedFilter,
 } from "./handlersSideBar";
-import { Icon } from "@iconify/react";
 import swal from "sweetalert";
 
 import styles from "./sidebar.module.css";
@@ -18,6 +18,26 @@ export default function Sidebar() {
   const filtros = useSelector((state) => state.filters);
   const navigate = useNavigate();
   const dispatch = useDispatch();
+  const [isMobile, setIsMobile] = useState(false);
+  const [isExpanded, setIsExpanded] = useState(false);
+  const toggleSidebar = () => {
+    setIsExpanded(!isExpanded);
+  };
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 768px)");
+    setIsMobile(mediaQuery.matches);
+
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
 
   let value = "default";
 
@@ -48,85 +68,173 @@ export default function Sidebar() {
   };
 
   return (
-    <div className={styles.container}>
-      <select
-        onChange={(e) => handleSelectedFilter(e, filtros, dispatch)}
-        className={styles.selectBox}
-        name="species"
-        defaultValue={value}
-      >
-        <option value="default" disabled hidden>
-          Animal
-        </option>
-        <option value="perro">Perro</option>
-        <option value="gato">Gato</option>
-        {/* <option value="conejo">Conejo</option>
-                  <option value="tortuga">Tortuga</option>
-                  <option value="cobayo">Cobayo</option> */}
-      </select>
-      <select
-        defaultValue={value}
-        onChange={(e) => handleSelectedFilter(e, filtros, dispatch)}
-        className={styles.selectBox}
-        name="size"
-      >
-        <option value="default" disabled hidden>
-          Tamaño
-        </option>
-        <option value="pequeño">Pequeño</option>
-        <option value="mediano">Mediano</option>
-        <option value="grande">Grande</option>
-      </select>
-      <select
-        defaultValue={value}
-        onChange={(e) => handleSelectedFilter(e, filtros, dispatch)}
-        className={styles.selectBox}
-        name="sex"
-      >
-        <option value="default" disabled hidden>
-          Sexo
-        </option>
-        <option value="macho">Macho</option>
-        <option value="hembra">Hembra</option>
-      </select>
-      {/* <select className={styles.selectBox} name="age" defaultValue={value}>
-      <option value="default" disabled hidden>Edad</option>
-          <option value="menor1">Menor de un año</option>
-          <option value="1a2">Entre uno  y dos años</option>
-          <option value="2plus">Más de dos años</option>
-      </select> */}
-      <Ordenamientos />
-      <button
-        name="delete filters"
-        onClick={(e) => handleCleanFilter(e, filtros, dispatch)}
-        className={styles.btn}
-      >
-        Restablecer Filtros
-      </button>
-      <form onSubmit={inputSearch}>
-        <div className={styles.searchContainer}>
-          <div className={styles.newsearch}>
-            <input
-              type="text"
-              placeholder="Buscar mascotas de:"
-              value={searchTerm}
-              onChange={handleSearch}
-              className={styles.search}
-            />
-            <button
-              name="name"
-              value={searchTerm}
-              onClick={inputSearch}
-              className={styles.searchButton}
+    <div>
+      {isMobile ? (
+        <div className={`${styles.sidebar} ${isExpanded ? styles.expanded : ''}`}>
+          <div className={styles.container}>
+            <select
+              onChange={(e) => handleSelectedFilter(e, filtros, dispatch)}
+              className={styles.selectBox}
+              name="species"
+              defaultValue={value}
             >
-              <Icon icon="fa6-solid:magnifying-glass" />
+              <option value="default" disabled hidden>
+                Animal
+              </option>
+              <option value="perro">Perro</option>
+              <option value="gato">Gato</option>
+              {/* <option value="conejo">Conejo</option>
+                    <option value="tortuga">Tortuga</option>
+                    <option value="cobayo">Cobayo</option> */}
+            </select>
+            <select
+              defaultValue={value}
+              onChange={(e) => handleSelectedFilter(e, filtros, dispatch)}
+              className={styles.selectBox}
+              name="size"
+            >
+              <option value="default" disabled hidden>
+                Tamaño
+              </option>
+              <option value="pequeño">Pequeño</option>
+              <option value="mediano">Mediano</option>
+              <option value="grande">Grande</option>
+            </select>
+            <select
+              defaultValue={value}
+              onChange={(e) => handleSelectedFilter(e, filtros, dispatch)}
+              className={styles.selectBox}
+              name="sex"
+            >
+              <option value="default" disabled hidden>
+                Sexo
+              </option>
+              <option value="macho">Macho</option>
+              <option value="hembra">Hembra</option>
+            </select>
+            {/* <select className={styles.selectBox} name="age" defaultValue={value}>
+        <option value="default" disabled hidden>Edad</option>
+            <option value="menor1">Menor de un año</option>
+            <option value="1a2">Entre uno  y dos años</option>
+            <option value="2plus">Más de dos años</option>
+        </select> */}
+            <Ordenamientos />
+            <button
+              name="delete filters"
+              onClick={(e) => handleCleanFilter(e, filtros, dispatch)}
+              className={styles.btn}
+            >
+              Restablecer Filtros
             </button>
+            <form onSubmit={inputSearch}>
+              <div className={styles.searchContainer}>
+                <div className={styles.newsearch}>
+                  <input
+                    type="text"
+                    placeholder="Buscar mascotas de:"
+                    value={searchTerm}
+                    onChange={handleSearch}
+                    className={styles.search}
+                  />
+                  <button
+                    name="name"
+                    value={searchTerm}
+                    onClick={inputSearch}
+                    className={styles.searchButton}
+                  >
+                    <Icon icon="fa6-solid:magnifying-glass" />
+                  </button>
+                </div>
+              <button className={styles.restoreButton} onClick={resetSearch}>
+                Eliminar Búsqueda
+              </button>
+            </div>
+            </form>
+          <button className={`${styles.toggle} ${isExpanded ? styles.visible : ''}`} onClick={toggleSidebar}><Icon icon="material-symbols:arrow-drop-down-circle-outline" /></button>
           </div>
-          <button className={styles.restoreButton} onClick={resetSearch}>
-            Eliminar Búsqueda
-          </button>
         </div>
-      </form>
+      ) : (
+        <div className={styles.container}>
+          <select
+            onChange={(e) => handleSelectedFilter(e, filtros, dispatch)}
+            className={styles.selectBox}
+            name="species"
+            defaultValue={value}
+          >
+            <option value="default" disabled hidden>
+              Animal
+            </option>
+            <option value="perro">Perro</option>
+            <option value="gato">Gato</option>
+            {/* <option value="conejo">Conejo</option>
+                    <option value="tortuga">Tortuga</option>
+                    <option value="cobayo">Cobayo</option> */}
+          </select>
+          <select
+            defaultValue={value}
+            onChange={(e) => handleSelectedFilter(e, filtros, dispatch)}
+            className={styles.selectBox}
+            name="size"
+          >
+            <option value="default" disabled hidden>
+              Tamaño
+            </option>
+            <option value="pequeño">Pequeño</option>
+            <option value="mediano">Mediano</option>
+            <option value="grande">Grande</option>
+          </select>
+          <select
+            defaultValue={value}
+            onChange={(e) => handleSelectedFilter(e, filtros, dispatch)}
+            className={styles.selectBox}
+            name="sex"
+          >
+            <option value="default" disabled hidden>
+              Sexo
+            </option>
+            <option value="macho">Macho</option>
+            <option value="hembra">Hembra</option>
+          </select>
+          {/* <select className={styles.selectBox} name="age" defaultValue={value}>
+        <option value="default" disabled hidden>Edad</option>
+            <option value="menor1">Menor de un año</option>
+            <option value="1a2">Entre uno  y dos años</option>
+            <option value="2plus">Más de dos años</option>
+        </select> */}
+          <Ordenamientos />
+          <button
+            name="delete filters"
+            onClick={(e) => handleCleanFilter(e, filtros, dispatch)}
+            className={styles.btn}
+          >
+            Restablecer Filtros
+          </button>
+          <form onSubmit={inputSearch}>
+            <div className={styles.searchContainer}>
+              <div className={styles.newsearch}>
+                <input
+                  type="text"
+                  placeholder="Buscar mascotas de:"
+                  value={searchTerm}
+                  onChange={handleSearch}
+                  className={styles.search}
+                />
+                <button
+                  name="name"
+                  value={searchTerm}
+                  onClick={inputSearch}
+                  className={styles.searchButton}
+                >
+                  <Icon icon="fa6-solid:magnifying-glass" />
+                </button>
+              </div>
+              <button className={styles.restoreButton} onClick={resetSearch}>
+                Eliminar Búsqueda
+              </button>
+            </div>
+          </form>
+        </div>
+      )}
     </div>
   );
 }
