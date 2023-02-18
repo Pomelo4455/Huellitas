de9115b5243e5f6a7dba7fdc4d68163c651ed834
Melina Fuch <sosafuchmelina@gmail.com>
2023-02-18 13:29:23 -0300

    link para ir a perfil de fundacion en /fundaciones y en las cards de home

--- client/src/components/Card/CardFundacion.jsx
+++ client/src/components/Card/CardFundacion.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import styles from "./CardFundacion.module.css";
+import { Link } from "react-router-dom";
 
 export default function CardFundacion({ fundacion }) {
   return (
     <div className={styles.card}>
       <div>
+        <Link to={`/userDetail/${fundacion.id}`} className={styles.link_detail} >
           <img src={fundacion.image} alt={fundacion.name} className={styles.img} />
           <h1 className={styles.title}>{fundacion.name}</h1>
           <h2 className={styles.text}>{fundacion.address}</h2>
+        </Link>
       </div>
     </div>
   );
