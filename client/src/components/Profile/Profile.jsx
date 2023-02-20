c5e45b4747fe68d4cfd6ca5ca5b03a0453fa46c4
Tomás Albanesi <tmsalbanesi@gmail.com>
2023-02-20 17:29:32 -0300

    mascotas y campañas al perfil

--- client/src/components/Profile/Profile.jsx
+++ client/src/components/Profile/Profile.jsx
@@ -11,6 +11,9 @@ import MapView from "../MapView/MapView";
 import axios from "axios";
 import swal from "sweetalert";
 import {useNavigate, navigate } from 'react-router-dom';
+import RenderizarEnAdopcion from './RenderEnAdopcion';
+import RenderizarAdoptados from './RenderAdoptados';
+import RenderizarCampaigns from './RenderCampaigns';
 
 
 
@@ -38,7 +41,9 @@ const Profile = ({setLoggedUser}) => {
       image: userId.data.image,
       adopciones: "No realizaste adopciones",
       donaciones: "No realizaste donaciones",
-      status: userId.data.status
+      status: userId.data.status,
+      type: userId.data.type,
+      id: userId.data.id
     });
     const handleSendMail = async () => {
       try {
@@ -163,8 +168,10 @@ const Profile = ({setLoggedUser}) => {
           </React.Fragment>
         )}
       </div>
-      <MapView />
-
+      {/* latitud && longitud && <MapView latitud longitud /> */}
+      <RenderizarEnAdopcion user={formData}/>
+      {/* <RenderizarAdoptados/> */}
+      {formData.type === "fundacion" && <RenderizarCampaigns user={formData}/>}
     </div>
     </div>
       
