920ed3f928d1d7d213d1d25c369f61d58429808f
friasae <friasae123@gmail.com>
2023-02-21 16:27:21 -0300

    nuevo railway

--- client/src/Utils/variablesDeploy.js
+++ client/src/Utils/variablesDeploy.js
@@ -2,6 +2,6 @@
 // const LINK_BACK="http://localhost:3001"
 
 const LINK_FRONT="https://huellitasapp.netlify.app"
-const LINK_BACK="https://huellitas-production.up.railway.app"
+const LINK_BACK="https://huellitas-production-f7c9.up.railway.app"
 
 module.exports = {LINK_BACK, LINK_FRONT};
