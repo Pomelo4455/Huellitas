5bb5b61fadc7696baf2dc9ac870ffce6c6be92f9
friasae <friasae123@gmail.com>
2023-02-22 11:53:43 -0300

    redirect fix

--- client/src/components/LogoutButton/LogoutButton.jsx
+++ client/src/components/LogoutButton/LogoutButton.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import { useDispatch } from "react-redux";
 import { login_failure } from "../../redux/actions";
+import { LINK_FRONT } from "../../Utils/variablesDeploy";
 
 import styles from "./LogoutButton.module.css";
 
@@ -9,7 +10,7 @@ const LogoutButton = () => {
   const dispatch = useDispatch();
   const { logout } = useAuth0();
   const handleinput = () => {
-    logout({ returnTo: "https://huellitasapp.netlify.app" });
+    logout({ returnTo: LINK_FRONT });
     localStorage.setItem("loggedUser", JSON.stringify({}));
     dispatch(login_failure());
   };
