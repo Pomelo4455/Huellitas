3b379fafea9dc4b64fcf196a7a1184544ef64f74
Tomás Albanesi <tmsalbanesi@gmail.com>
2023-02-23 02:58:54 -0300

    profile full responsive

--- client/src/components/Profile/RenderCampaigns.jsx
+++ client/src/components/Profile/RenderCampaigns.jsx
@@ -3,7 +3,8 @@ import React, { useEffect, useState } from "react";
 import Campaña from "../Campaigns/Campaing";
 import { LINK_BACK } from "../../Utils/variablesDeploy";
 import './RenderCampaings.css'
-import { Icon } from '@iconify/react'
+import styles from "../Home/home.module.css"
+
 
 const renderizarCampaignsCards = (allCampaigns, cantidad) => {
     return allCampaigns.slice(0, cantidad).map((camp) => {
@@ -38,23 +39,21 @@ export default function RenderizarCampaigns({user}) {
     if (campaigns.length > 1) {
     return (
         <>
-        
         <h2 >Campañas</h2>
-        
-        <div className="campañascontainer">
         {more ? 
-                <>
-                {renderizarCampaignsCards(campaigns, campaigns.length)}
-                <button className="buttonCamapañasView" onClick={handleShowMore}><Icon icon="material-symbols:keyboard-double-arrow-left-sharp" /></button>
-                </>
-            :
-                <>
-                {renderizarCampaignsCards(campaigns, 1)}
-                <button className="buttonCamapañasView" onClick={handleShowMore}> <Icon icon="material-symbols:keyboard-double-arrow-right" /></button>
-                </>
-            }
-        </div>
-        
+          <div className={styles.cards}>
+          {renderizarCampaignsCards(campaigns, campaigns.length)}
+          </div>
+        :
+          <div className={styles.cards}>
+          {renderizarCampaignsCards(campaigns, 1)}
+          </div>
+        }
+        {more ?
+          <button className={styles.claseboton} onClick={handleShowMore}>ʌ</button>
+        :
+          <button className={styles.claseboton} onClick={handleShowMore}>v</button>
+        }
         </>
     )
 }
